15bc80933e27de885d8b461f4cb91a03110601f0
Zhao <30463691+Sniper970119@users.noreply.github.com>
2019-02-16 23:30:27 +0800

    update message transfer system

--- src/Client/Conf/config.py
+++ src/Client/Conf/config.py
@@ -18,12 +18,14 @@ import win32con
 
 
 # 调试模式总开关
-DEBUG = False
+DEBUG = True
 # GUI部分调试开关
 VIEW_DEBUG = True
 # 任务系统调试开关
 MISSION_DEBUG = True
 # 搜索系统调试开关
 SEARCH_DEBUG = True
+# 系统工具调试开关
+SYSTEM_TOOLS_DEBUG = True
 
 
---
+++ src/Client/SystemTools/ConfFileRead/configFileRead.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+
+
+from src.Client.Conf.config import *
+from src.Client.SystemTools.ConfFileRead.Tools import readConfigFile
+from src.Client.SystemTools.ConfFileRead.Tools import saveConfigFile
+
+
+class ConfigFileRead():
+    def __init__(self, fileName='../conf/main.ini'):
+        self.readConfigFileTools = readConfigFile.ReadConfigFile(fileName=fileName)
+        self.saveConfigFileTools = saveConfigFile.SaveConfigFile(fileName=fileName)
+        pass
+
+    def readFile(self, configMainName, configSubName):
+        """
+        读取配置文件
+        :param configMainName: 配置文件主名称
+        :param configSubName: 配置文件副名称
+        :return: 配置信息的值
+        """
+        message = self.readConfigFileTools.readConfigFile(configMainName=configMainName, configSubName=configSubName)
+        return message
+
+    def saveFile(self, configMainName, configSubName, value):
+        """
+        保存到配置文件
+        :param configMainName: 配置文件主名称
+        :param configSubName: 配置文件副名称
+        :param value: 配置文件的值
+        :return:
+        """
+        self.saveConfigFileTools.saveConfigFile(configMainName=configMainName, configSubName=configSubName, value=value)
+
