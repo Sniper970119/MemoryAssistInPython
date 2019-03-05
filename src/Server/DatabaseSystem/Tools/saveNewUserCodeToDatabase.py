b56ff375ce7b63254b7c907a330cae940f96b25f
Zhao <30463691+Sniper970119@users.noreply.github.com>
2019-03-05 22:13:51 +0800

    update server  save to database

--- src/Server/DatabaseSystem/Tools/saveNewUserCodeToDatabase.py
+++ src/Server/DatabaseSystem/Tools/saveNewUserCodeToDatabase.py
@@ -5,7 +5,8 @@ from src.Server.Conf.config import *
 class SaveNewUserCodeToDataBase():
     def __init__(self):
         myclient = pymongo.MongoClient("localhost:27017")
-        self.mydb = myclient["runoobdb"]
+        mydb = myclient["MemoryAssist"]
+        self.mycol = mydb["user"]
         pass
 
     def save(self, code):
